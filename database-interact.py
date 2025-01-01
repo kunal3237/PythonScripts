19f9915f75a8a227a0838b965b3f492f0d30af38
kunal3237 <kunal.sharma37@gmail.com>
2025-01-01 11:06:06 +0530

    Update database-interact.py

--- database-interact.py
+++ database-interact.py
@@ -1,9 +1,13 @@
 """
 Expected Output
 ____________________________
-Enter the password..: 
-Enter the db_name..: 
+Data Deleted..
+Data Added
 ('John', 'Highway 21')
+('Peter', 'Lowstreet 4')
+('Iphone', 'America')
+('Iphone', 'America')
+('Iphone', 'America')
 """
 
 #Author: Kunal Sharma
@@ -12,24 +16,36 @@ import mysql.connector
 import os
 import getpass
 
-def database_play():
-            psd=getpass.getpass(prompt="Enter the password..: ")
-            db_name=getpass.getpass(prompt="Enter the db_name..: ")
-            my_con=mysql.connector.connect(host='localhost',user='oscar',password=psd,database=db_name)
-            mycon_cur=my_con.cursor()
-            #mycon_cur.execute("select * from customers")
-            insert_query="INSERT INTO customers (name, address) VALUES( %s, %s)"
-            data_to_insert=( 'Kunal','Palampur')
-            mycon_cur.execute(insert_query,data_to_insert)
-            my_con.commit()
-            select_query="select * from customers where name= %s"
-            select_var=["John"]
-            mycon_cur.execute(select_query,select_var)
-            myresult=mycon_cur.fetchone()
-            while myresult:
-                print(myresult)
+def select():
+
+            with mysql.connector.connect(host='localhost',user=os.getenv('dbuser'),password=os.getenv('psd'),database=os.getenv('db_name')) as connection:
+                mycon_cur=connection.cursor()
+                insert_query="INSERT INTO customers (name, address) VALUES( %s, %s)"
+                select_query="select * from customers" # where name= %s"
+                mycon_cur.execute(select_query) #select_var
                 myresult=mycon_cur.fetchone()
-            #print(help(my_con.cursor()))
-            # print(dir(mysql.connector))
-            
-database_play()            
+                while myresult:
+                    print(myresult)
+                    myresult=mycon_cur.fetchone()
+                    
+def insert():
+            with mysql.connector.connect(host='localhost',user=os.getenv('dbuser'),database=os.getenv('db_name'),password=os.getenv('psd')) as connection:
+                mycon_cur=connection.cursor()
+                insert_query="insert into customers(name,address)  values(%s,%s)"
+                insert_var=('Iphone','America')
+                mycon_cur.execute(insert_query,insert_var)
+                connection.commit()
+                print("Data Added")
+                
+def delete():
+            with mysql.connector.connect(host="localhost",user=os.getenv('dbuser'),password=os.getenv('psd'),database=os.getenv('db_name')) as connection:
+                mycon_cur=connection.cursor()
+                delete_query="delete from customers where name=%s"
+                delete_var=["Kunal"]
+                mycon_cur.execute(delete_query,delete_var)
+                connection.commit()
+                print("Data Deleted..")                
+                        
+delete()
+insert()            
+select()            
