86fc0f8abfde6f20573b45f13af8553249faa9b1
kunal3237 <kunal.sharma37@gmail.com>
2025-01-02 10:49:21 +0530

    Update database-interact-mysql.py

--- database-interact-mysql.py
+++ database-interact-mysql.py
@@ -2,19 +2,6 @@
 Expected Output
 ____________________________
 Welcome to Mysql kunal database 
-Data Deleted..
-Data Added
-('John', 'Highway 21')
-('Peter', 'Lowstreet 4')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
-('Iphone', 'America')
 ('mysql', '2.64Mb', '-137.36Mb', '140.00Mb', Decimal('-5201.78'))
 ('sys', '0.02Mb', '0.02Mb', '0.00Mb', Decimal('100.00'))
 ('kunal', '0.02Mb', '0.02Mb', '0.00Mb', Decimal('100.00'))
@@ -35,55 +22,58 @@ class DatabaseCheck:
             def select(self):
 
                         with mysql.connector.connect(host='localhost',user=os.getenv('dbuser'),password=os.getenv('psd'),database=os.getenv('db_name')) as connection:
-                            mycon_cur=connection.cursor()
-                            insert_query="INSERT INTO customers (name, address) VALUES( %s, %s)"
-                            select_query="select * from customers" # where name= %s"
-                            mycon_cur.execute(select_query) #select_var
-                            myresult=mycon_cur.fetchone()
-                            while myresult:
-                                print(myresult)
+                            with connection.cursor() as mycon_cur:
+                                insert_query="INSERT INTO customers (name, address) VALUES( %s, %s)"
+                                select_query="select * from customers" # where name= %s"
+                                mycon_cur.execute(select_query) #select_var
                                 myresult=mycon_cur.fetchone()
+                                while myresult:
+                                    print(myresult)
+                                    myresult=mycon_cur.fetchone()
+                                connection.commit()
                                 
             def insert(self):
                         with mysql.connector.connect(host='localhost',user=os.getenv('dbuser'),database=os.getenv('db_name'),password=os.getenv('psd')) as connection:
-                            mycon_cur=connection.cursor()
-                            insert_query="insert into customers(name,address)  values(%s,%s)"
-                            insert_var=('Iphone','America')
-                            mycon_cur.execute(insert_query,insert_var)
-                            connection.commit()
-                            print("Data Added")
+                            with connection.cursor() as mycon_cur:
+                                insert_query="insert into customers(name,address)  values(%s,%s)"
+                                insert_var=('Iphone','America')
+                                mycon_cur.execute(insert_query,insert_var)
+                                connection.commit()
+                                print("Data Added")
+                        connection.commit()    
                             
             def delete(self):
                         with mysql.connector.connect(host="localhost",user=os.getenv('dbuser'),password=os.getenv('psd'),database=os.getenv('db_name')) as connection:
-                            mycon_cur=connection.cursor()
-                            delete_query="delete from customers where name=%s"
-                            delete_var=["Kunal"]
-                            mycon_cur.execute(delete_query,delete_var)
-                            connection.commit()
-                            print("Data Deleted..")                
+                            with connection.cursor() as mycon_cur:
+                                delete_query="delete from customers where name=%s"
+                                delete_var=["Kunal"]
+                                mycon_cur.execute(delete_query,delete_var)
+                                connection.commit()
+                                print("Data Deleted..")  
+                            connection.commit()                  
                     
             def database_usage(self):
                         with mysql.connector.connect(host="localhost",user=os.getenv('dbuser'),password=os.getenv('psd'),database=os.getenv('db_name')) as connection:
-                            mycon_cur=connection.cursor()
-                            database_query="""SELECT s.schema_name,
-                            CONCAT(IFNULL(ROUND((SUM(t.data_length)+SUM(t.index_length))/1024/1024,2),0.00),"Mb") total_size,
-                            CONCAT(IFNULL(ROUND(((SUM(t.data_length)+SUM(t.index_length))-SUM(t.data_free))/1024/1024,2),0.00),"Mb") data_used,
-                            CONCAT(IFNULL(ROUND(SUM(data_free)/1024/1024,2),0.00),"Mb") data_free,
-                            IFNULL(ROUND((((SUM(t.data_length)+SUM(t.index_length))-SUM(t.data_free))/((SUM(t.data_length)+SUM(t.index_length)))*100),2),0) pct_used
-                            FROM INFORMATION_SCHEMA.SCHEMATA s, INFORMATION_SCHEMA.TABLES t
-                            WHERE s.schema_name = t.table_schema
-                            GROUP BY s.schema_name
-                            ORDER BY total_size DESC"""
-                            mycon_cur.execute(database_query)
-                            result=mycon_cur.fetchone()
-                            while result:
-                                print(result)
-                                result= mycon_cur.fetchone()               
+                            with connection.cursor() as mycon_cur:
+                                database_query="""SELECT s.schema_name,
+                                CONCAT(IFNULL(ROUND((SUM(t.data_length)+SUM(t.index_length))/1024/1024,2),0.00),"Mb") total_size,
+                                CONCAT(IFNULL(ROUND(((SUM(t.data_length)+SUM(t.index_length))-SUM(t.data_free))/1024/1024,2),0.00),"Mb") data_used,
+                                CONCAT(IFNULL(ROUND(SUM(data_free)/1024/1024,2),0.00),"Mb") data_free,
+                                IFNULL(ROUND((((SUM(t.data_length)+SUM(t.index_length))-SUM(t.data_free))/((SUM(t.data_length)+SUM(t.index_length)))*100),2),0) pct_used
+                                FROM INFORMATION_SCHEMA.SCHEMATA s, INFORMATION_SCHEMA.TABLES t
+                                WHERE s.schema_name = t.table_schema
+                                GROUP BY s.schema_name
+                                ORDER BY total_size DESC"""
+                                mycon_cur.execute(database_query)
+                                result=mycon_cur.fetchone()
+                                while result:
+                                    print(result)
+                                    result= mycon_cur.fetchone()               
                         
 
 database_check=DatabaseCheck()
-database_check.delete()
-database_check.insert()
-database_check.select()
+# database_check.delete()
+# database_check.insert()
+# database_check.select()
 database_check.database_usage()
 
