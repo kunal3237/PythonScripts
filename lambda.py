3f7e92f8793ae730ba11c88e7ab7f0388e10e1d1
kunal3237 <kunal.sharma37@gmail.com>
2026-02-05 22:02:45 +0800

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,3 +1,5 @@
+### nonlocal and global keywords should be used when there is need to modify the varibles in local scope. Also do remember LEGB scope
+
 #"""lambda function, For working with lambda function we need to assign it s0me varaible"
 # Example with no variable
 first_name=lambda : 'kunal'
@@ -79,7 +81,8 @@ def funscope1():
 
 funscope1()  
 
-#######################################def funscope2():
+#######################################
+def funscope2():
   
   #global x
   x=5
@@ -94,7 +97,8 @@ def funscope2():
   x=5
   return x
 funscope2()  
-print(x)                    # this will be ok as be are using global keyword and it will pass the variable globally
+print(x)                    # this will be ok as be are using global keyword and it will pass the variable globally, with global keyword we can modify the global scope 
+                            #variable
 
 ############################################################
 x=5
@@ -103,4 +107,62 @@ def func1():
   return x
 
 func1()
-print(x)  
+print(x)  # this wll print different variables as both have different scopes
+
+#####################################################
+#Nested Function
+def func2():
+  def innerfunc2():
+    return f'This is from inner function'
+  print( f'This is from outer function')
+  return innerfunc2
+k=func2() #function innerfunc2 is stored in the variable, Need to call the function  
+k()   
+#######################################
+
+def outer():
+  name='kunal'
+  def inner():
+    return f'This function is using the variable {name} from outer function'
+  return inner  
+
+s=outer()                      #### Outer variable being used by inner function
+s()
+#############################################
+def outer():
+  name5='kunal'
+  def inner():
+    nonlocal name5      ## nonlocal is used for enclosed variable, Immediate outer function. This is used to modify the outer function variable
+    name5=name5 + ' Sharma'
+    print( f'This function is using the variable {name5} from outer function')
+    return name5
+  return inner  
+
+s=outer()
+s()
+################################################
+del outer1
+def outer1():
+  name='sharma'
+  def inner1():
+    name=name+'palam'          ### THis will make function fail as inner1 function is looking for varaible name, so if you wan to modify the varaible use nonlocal
+    return f'This is from inner function {name}'
+  print(f'This is from outer function {name}')
+  return inner1
+
+l=outer1()
+l()
+
+# Below one is working as we are using nonlocal
+del outer1
+def outer1():
+  name='sharma'
+  def inner1():
+    nonlocal name
+    name=name+'palam'
+    return f'This is from inner function {name}'
+  print(f'This is from outer function {name}')
+  return inner1
+
+l=outer1()
+l()
