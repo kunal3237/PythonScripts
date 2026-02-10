a8b8c492acc5ddc81a0bd82049608d92f4e33e85
kunal3237 <kunal.sharma37@gmail.com>
2026-02-10 15:44:09 +0800

    Update Learn-Class1.py

--- Learn-Class1.py
+++ Learn-Class1.py
@@ -1,29 +1,49 @@
 #####################################################
+### Encapsulation example (with Protected _ and Private attributes __ ). Here er are controlling the acces of the attributes
 class Seller:
-  def __init__(self,name,address,gstin,myinventory=[]):      #### This is not good approach as mutable types give problem with default values. It should
-    self.name=name                                            #### Be tested
+  def __init__(self,name,address,gstin,tax):      ###Constructor
+    self.name=name
     self.address=address
-    self.gstin=gstin
-    self.myinventory=myinventory
+    self._gstin=gstin         ###Protected
+    self.__tax=tax            ####Private
+
+  @property                   # Created a getter method to contorl the access of the attribute,even though it protected can be changed
+  def gstin(self):        #externally. 
+    return f'{self._gstin}'
+
+  @property                 ###Getter method for Private Attribute. This is Private, it can be changed and access externally
+  def tax(self):        
+    return f'{self.__tax}'  #### Name mangled to seller1._Seller__tax i.e obj._Class__attribute
+
+  @tax.setter         ### Name should match with getter name,even the name of the function being used in the getter and setter
+  def tax(self,new_tax):
+      self.__tax=new_tax
+      return f'{self.__tax}'    ### This is for testing. 
+
+  def sell(self):
+    return f'I am selling'
+
+  def add_inventory(self):
+    return f'Added in the inventory'
+
+  def check_inventory(self):
+    return f'Checking in the inventory'    
 
 
-  def addInventory(self,product):
-      self.myinventory.append(product)
-      return self.myinventory
+seller1=Seller('kunal','Palampur',244,5453)
+print(seller1._gstin) 
+seller1._gstin=455 
+print(seller1._gstin)    ### Attribute is protected ,even then it can be modified from outside
+print(seller1.gstin)
+#print(seller1.__tax)    ### This will complain as it's name is mangled, can't be access 
+#print(seller1._Seller__tax)
+#seller1.__tax=4343
+#print(seller1.__tax)    ### It is changed because now it is new attribute
+#print(dir(seller1))
+print(seller1.tax)
+seller1.tax=4355354     ## once you configure getter and setter. Make sure you use those as attribute not as a function
+print(seller1.tax)
 
-  def checkInventory(self,product):
-      if product in self.myinventory:
-        cnt=self.myinventory.count(product)
-        return f'Product is available and have Quantity --> {cnt} '    
-  def sellProduct(self,product):
-    if product in self.myinventory:
-      self.myinventory.remove(product)
-      return f'self.checkInventory(product) , {self.myinventory}'
 
-s1=Seller('Kunal','Palampur','231213')
-s1.addInventory('fan')
-s1.addInventory('Table')
-s1.addInventory('Almirah')
-s1.addInventory('fan')
-print(s1.checkInventory('fan'))
-print(s1.sellProduct('fan'))
+
+
