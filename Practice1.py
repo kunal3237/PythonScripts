4bae9dee56d1ff214e0d1aa63c66e2b12ea11b82
kunal3237 <kunal.sharma37@gmail.com>
2026-02-11 20:36:05 +0800

    Update Practice1.py

--- Practice1.py
+++ Practice1.py
@@ -10,3 +10,25 @@ class Student:
 callable(Student)       ##Class is always callable
 student1=Student()    
 callable(student1)
+
+######################################
+from abc import ABC,abstractmethod
+class Payment(ABC):
+  @abstractmethod
+  def pay():
+    pass
+
+class CreditCard(Payment):
+  def __init__(self,amount):
+    self.amount=amount
+
+  def pay(self):
+    return f'Amount paid {self.amount}'  
+  
+  
+  def amounToPay(self):
+    return f'Paying {self.amount}'
+
+cc1=CreditCard(3000)
+#cc1.amounToPay()
+cc1.pay()
